b1fee2886a5d0bdff10727edeeda57aa6d62c822
Rikuncy <49175023+Rikuncy@users.noreply.github.com>
2019-04-01 21:49:30 -0400

    Update and rename 2017-05-01.js to 2019-04-01.js

---
+++ src/js/data/2019-04-01.js
@@ -0,0 +1,158 @@
+dataSetVersion = "2019-04-01"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Series",
+    key: "series",
+    tooltip: "Check this to restrict to certain series.",
+    checked: false,
+    sub: [
+      { name: "Otogi no Uta", key: "Otogi" }
+    ]
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Momose",
+    img: "otogi-no-uta/images/5/5f/Momose_sp.png/revision/latest/scale-to-width-down/494?cb=20171123042017",
+    opts: {
+      series: ["Otogi"],
+    }
+  },
+  {
+    name: "Inutake",
+    img: "otogi-no-uta/images/2/28/Inutake_sp.png/revision/latest/scale-to-width-down/494?cb=20171123042032",
+    opts: {
+      series: ["Otogi"],
+    }
+  },
+  {
+    name: "Saruhashi",
+    img: "otogi-no-uta/images/0/07/Saruhashi_sp.png/revision/latest/scale-to-width-down/494?cb=20171123042055",
+    opts: {
+      series: ["Otogi"],
+  },
+  {
+    name: "Torisawa",
+    img: "otogi-no-uta/images/2/20/Torisawa_sp.png/revision/latest/scale-to-width-down/494?cb=20171123042104",
+    opts: {
+      series: ["Otogi"],
+  },
+  {
+    name: "Baron Diesbach",
+    img: "otogi-no-uta/images/2/2e/Ao_sp.png/revision/latest/scale-to-width-down/494?cb=20171123052244",
+    opts: {
+      series: ["Otogi"],
+    }
+  },
+  {
+    name: "René Junker",
+    img: "otogi-no-uta/images/7/70/Rene_sp.png/revision/latest/scale-to-width-down/494?cb=20171123052500",
+    opts: {
+      series: ["Otogi"],
+  },
+  {
+    name: "Kan",
+    img: "otogi-no-uta/images/8/87/Kan_sp.png/revision/latest/scale-to-width-down/494?cb=20171123063038",
+    opts: {
+      series: ["Otogi"],
+  },
+  {
+    name: "Nono",
+    img: "otogi-no-uta/images/a/a9/Nono_sp.png/revision/latest/scale-to-width-down/494?cb=20171123063123",
+    opts: {
+      series: ["Otogi"],
+    }
+  },
+  {
+    name: "Kumoo",
+    img: "otogi-no-uta/images/a/a4/Kumo_sp.png/revision/latest/scale-to-width-down/494?cb=20171123063229",
+    opts: {
+      series: ["Otogi"],
+    }
+  },
+  {
+    name: "Esel Nachtmusik",
+    img: "otogi-no-uta/images/5/55/Esel_sp.png/revision/latest/scale-to-width-down/494?cb=20171123073954",
+    opts: {
+      series: ["Otogi"],
+    }
+  },
+  {
+    name: "Katze Träumerei",
+    img: "otogi-no-uta/images/c/cb/Katze_sp.png/revision/latest/scale-to-width-down/494?cb=20171123074423",
+    opts: {
+      series: ["Otogi"],
+          }
+  },
+  {
+    name: "Vogel Konzert",
+    img: "otogi-no-uta/images/0/03/Vogel_sp.png/revision/latest/scale-to-width-down/494?cb=20171123083142",
+    opts: {
+      series: ["Otogi"],}
+  },
+  {
+    name: "Jakthund Symphonie",
+    img: "otogi-no-uta/images/3/38/Jakthund_sp.png/revision/latest/scale-to-width-down/494?cb=20171123083218",
+    opts: {
+      series: ["Otogi"],
+    }
+  },
+  {
+    name: "Snicksnicker",
+    img: "otogi-no-uta/images/f/f3/Snick_sp.png/revision/latest/scale-to-width-down/494?cb=20171123092651",
+    opts: {
+      series: ["Otogi"],
+    }
+  },
+  {
+    name: "Baggy☆Crow",
+    img: "otogi-no-uta/images/6/69/Bagy_sp.png/revision/latest/scale-to-width-down/494?cb=20171123093104",
+    opts: {
+      series: ["Otogi"],
+    }
+  },
+  {
+    name: "Alistair",
+    img: "otogi-no-uta/images/f/fe/Alice_sp.png/revision/latest/scale-to-width-down/494?cb=20171123093322",
+    opts: {
+      series: ["Otogi"],
+    }
+  },
+  {
+    name: "Muumuu",
+    img: "otogi-no-uta/images/8/86/Mu_sp.png/revision/latest/scale-to-width-down/494?cb=20171123093545",
+    opts: {
+      series: ["Otogi"],
+    }
+  },
+  {
+    name: "Ootsuzura",
+    img: "otogi-no-uta/images/1/11/Otudura_sp.png/revision/latest/scale-to-width-down/494?cb=20171123095746",
+    opts: {
+      series: ["Otogi"],
+    }
+  },
+  {
+    name: "Yosuzume",
+    img: "otogi-no-uta/images/c/ce/Yosuzume_sp.png/revision/latest/scale-to-width-down/494?cb=20171123095913",
+    opts: {
+      series: ["Otogi"],
+    }
+  },
+  {
+    name: "Kotsuzura",
+    img: "otogi-no-uta/images/9/90/Kotudura_sp.png/revision/latest/scale-to-width-down/494?cb=20171123100031",
+    opts: {
+      series: ["Otogi"],
+    }
+  },
+  {
+    name: "Issun-bōshi",
+    img: "otogi-no-uta/images/0/0d/Issun_sp_pre.png/revision/latest/scale-to-width-down/494?cb=20180215073405",
+    opts: {
+      series: ["Otogi"],
+  }
+];
